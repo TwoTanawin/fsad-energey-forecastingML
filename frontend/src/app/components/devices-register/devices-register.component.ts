6ecc35731b2c90451791968dcd26318f56965737
twotanawin <tanawin.twox@gmail.com>
2024-11-17 02:45:31 +0700

    completed get token

--- frontend/src/app/components/devices-register/devices-register.component.ts
+++ frontend/src/app/components/devices-register/devices-register.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { DeviceService } from '../../services/device.service';
 
 @Component({
   selector: 'app-devices-register',
   templateUrl: './devices-register.component.html',
-  styleUrl: './devices-register.component.scss'
+  styleUrls: ['./devices-register.component.scss']
 })
 export class DevicesRegisterComponent {
+  deviceAddress: string = '';
+  token: string | null = null;
 
+  constructor(private deviceService: DeviceService) {}
+
+  registerDevice() {
+    if (this.deviceAddress) {
+      // Call the service to register the device
+      this.deviceService.registerDevice(this.deviceAddress).subscribe({
+        next: (response) => {
+          console.log('Device registered successfully:', response);
+          if (response.token) {
+            this.token = response.token; // Display the new token
+          } else {
+            alert('Device registered, but no token received.');
+          }
+        },
+        error: (error) => {
+          console.error('Error registering device:', error);
+          alert('Failed to register device.');
+        }
+      });
+    } else {
+      alert('Please provide a device address.');
+    }
+  }
 }
